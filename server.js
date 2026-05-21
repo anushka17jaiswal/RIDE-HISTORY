b1d0654e9744e7e954b3916750400fba4fd43c19
Anushka Jaiswal <nush1@Anushkas-MacBook-Pro.local>
2026-05-21 18:35:34 +0530

    Added rides routes

--- server.js
+++ server.js
@@ -24,6 +24,39 @@ db.connect((err) => {
 app.get("/", (req, res) => {
     res.send("Ride History Backend Running");
 });
+app.get("/rides", (req, res) => {
+    db.query("SELECT * FROM rides", (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error fetching rides");
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.post("/rides", (req, res) => {
+    const { date, pickup, drop, fare, status } = req.body;
+
+    const sql = `
+        INSERT INTO rides 
+        (ride_date, pickup_location, drop_location, fare, status)
+        VALUES (?, ?, ?, ?, ?)
+    `;
+
+    db.query(
+        sql,
+        [date, pickup, drop, fare, status],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send("Insert failed");
+            } else {
+                res.send("Ride added");
+            }
+        }
+    );
+});
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
